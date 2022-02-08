436f1c44da0b9f6eb4f47d365eafccc02b2780f7
hhimely <hhimely@gmail.com>
2022-02-08 13:19:15 -0500

    Add project files.

---
+++ XafWebDemo.Module/DatabaseUpdate/Updater.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Security.Strategy;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using XafWebDemo.Module.BusinessObjects;
+using Bogus;
+
+namespace XafWebDemo.Module.DatabaseUpdate {
+    // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Updating.ModuleUpdater
+    public class Updater : ModuleUpdater {
+        public Updater(IObjectSpace objectSpace, Version currentDBVersion) :
+            base(objectSpace, currentDBVersion) {
+        }
+        public override void UpdateDatabaseAfterUpdateSchema() {
+            base.UpdateDatabaseAfterUpdateSchema();
+
+            if (ObjectSpace.GetObjectsCount(typeof(Customer), null) == 0)
+            {
+                var customerFaker = new Faker<Customer>()
+                    .CustomInstantiator(f => new Customer(((XPObjectSpace)ObjectSpace).Session))
+                    .RuleFor(o => o.FirstName, f => f.Name.FirstName())
+                    .RuleFor(o => o.Lastname, f => f.Name.LastName())
+                    .RuleFor(o => o.Address, f => f.Address.FullAddress())
+                    .RuleFor(o => o.City, f => f.Address.City())
+                    .RuleFor(o => o.Country, f => f.Address.Country())
+                    .RuleFor(o => o.PostalCode, f => f.Address.CountryCode())
+                    .RuleFor(o => o.PhoneNumber, f => f.Phone.PhoneNumber())
+                    .RuleFor(o => o.Email, f => f.Internet.Email())
+                    .RuleFor(o => o.Birthday, f => f.Date.Recent(100));
+
+                var invoiceFaker = new Faker<Invoice>()
+                    .CustomInstantiator(f => new Invoice(((XPObjectSpace)ObjectSpace).Session))
+                    .RuleFor(o => o.InvoiceNumber, f => f.Random.AlphaNumeric(10))
+                    .RuleFor(o => o.Description, f => f.Commerce.ProductMaterial())
+                    .RuleFor(o => o.InvoiceDate, f => f.Date.Recent());
+
+                System.Collections.Generic.List<Invoice> invoices = invoiceFaker.Generate(5000);
+                System.Collections.Generic.List<Customer> customers = customerFaker.Generate(5000);
+
+                foreach (var invoice in invoices)
+                {
+                    invoice.Customer = customers[invoices.IndexOf(invoice)];
+                }
+
+                ObjectSpace.CommitChanges();
+            }
+
+   
+            ApplicationUser sampleUser = ObjectSpace.FirstOrDefault<ApplicationUser>(u => u.UserName == "User");
+            if(sampleUser == null) {
+                sampleUser = ObjectSpace.CreateObject<ApplicationUser>();
+                sampleUser.UserName = "User";
+                // Set a password if the standard authentication type is used
+                sampleUser.SetPassword("");
+
+                // The UserLoginInfo object requires a user object Id (Oid).
+                // Commit the user object to the database before you create a UserLoginInfo object. This will correctly initialize the user key property.
+                ObjectSpace.CommitChanges(); //This line persists created object(s).
+                ((ISecurityUserWithLoginInfo)sampleUser).CreateUserLoginInfo(SecurityDefaults.PasswordAuthentication, ObjectSpace.GetKeyValueAsString(sampleUser));
+            }
+            PermissionPolicyRole defaultRole = CreateDefaultRole();
+            sampleUser.Roles.Add(defaultRole);
+
+            ApplicationUser userAdmin = ObjectSpace.FirstOrDefault<ApplicationUser>(u => u.UserName == "Admin");
+            if(userAdmin == null) {
+                userAdmin = ObjectSpace.CreateObject<ApplicationUser>();
+                userAdmin.UserName = "Admin";
+                // Set a password if the standard authentication type is used
+                userAdmin.SetPassword("");
+
+                // The UserLoginInfo object requires a user object Id (Oid).
+                // Commit the user object to the database before you create a UserLoginInfo object. This will correctly initialize the user key property.
+                ObjectSpace.CommitChanges(); //This line persists created object(s).
+                ((ISecurityUserWithLoginInfo)userAdmin).CreateUserLoginInfo(SecurityDefaults.PasswordAuthentication, ObjectSpace.GetKeyValueAsString(userAdmin));
+            }
+			// If a role with the Administrators name doesn't exist in the database, create this role
+            PermissionPolicyRole adminRole = ObjectSpace.FirstOrDefault<PermissionPolicyRole>(r => r.Name == "Administrators");
+            if(adminRole == null) {
+                adminRole = ObjectSpace.CreateObject<PermissionPolicyRole>();
+                adminRole.Name = "Administrators";
+            }
+            adminRole.IsAdministrative = true;
+			userAdmin.Roles.Add(adminRole);
+            ObjectSpace.CommitChanges(); //This line persists created object(s).
+        }
+        public override void UpdateDatabaseBeforeUpdateSchema() {
+            base.UpdateDatabaseBeforeUpdateSchema();
+            //if(CurrentDBVersion < new Version("1.1.0.0") && CurrentDBVersion > new Version("0.0.0.0")) {
+            //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
+            //}
+        }
+        private PermissionPolicyRole CreateDefaultRole() {
+            PermissionPolicyRole defaultRole = ObjectSpace.FirstOrDefault<PermissionPolicyRole>(role => role.Name == "Default");
+            if(defaultRole == null) {
+                defaultRole = ObjectSpace.CreateObject<PermissionPolicyRole>();
+                defaultRole.Name = "Default";
+
+				defaultRole.AddObjectPermissionFromLambda<PermissionPolicyUser>(SecurityOperations.Read, cm => cm.Oid == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
+                defaultRole.AddNavigationPermission(@"Application/NavigationItems/Items/Default/Items/MyDetails", SecurityPermissionState.Allow);
+				defaultRole.AddMemberPermissionFromLambda<PermissionPolicyUser>(SecurityOperations.Write, "ChangePasswordOnFirstLogon", cm => cm.Oid == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
+				defaultRole.AddMemberPermissionFromLambda<PermissionPolicyUser>(SecurityOperations.Write, "StoredPassword", cm => cm.Oid == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
+                defaultRole.AddTypePermissionsRecursively<PermissionPolicyRole>(SecurityOperations.Read, SecurityPermissionState.Deny);
+                defaultRole.AddTypePermissionsRecursively<ModelDifference>(SecurityOperations.ReadWriteAccess, SecurityPermissionState.Allow);
+                defaultRole.AddTypePermissionsRecursively<ModelDifferenceAspect>(SecurityOperations.ReadWriteAccess, SecurityPermissionState.Allow);
+				defaultRole.AddTypePermissionsRecursively<ModelDifference>(SecurityOperations.Create, SecurityPermissionState.Allow);
+                defaultRole.AddTypePermissionsRecursively<ModelDifferenceAspect>(SecurityOperations.Create, SecurityPermissionState.Allow);
+            }
+            return defaultRole;
+        }
+    }
+}
